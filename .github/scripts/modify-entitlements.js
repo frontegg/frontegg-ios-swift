9c65899746e7ff389131b12bc03e42c6eea05132
David Antoon <david@frontegg.com>
2023-04-26 19:09:39 +0300

    modify entitlemnet

--- .github/scripts/modify-entitlements.js
+++ .github/scripts/modify-entitlements.js
@@ -1,25 +1,29 @@
 module.exports = async ({github, fetch, fs}) => {
   const {writeFileSync} = require('fs')
+  const path = require('path')
   await new Promise(resolve => {
     const intervalRef = setInterval(async () => {
       const response = await fetch('http://localhost:4001/ngrok');
       const body = await response.text();
       if (body.startsWith("https://")) {
         clearInterval(intervalRef)
-        resolve(body);
 
-        console.log('ngrok YRL', body)
+        console.log('ngrok URL', body)
+
+          const host = (new URL("https://d645-109-65-152-21.ngrok-free.app")).host
         writeFileSync(`<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
 \t<key>com.apple.developer.associated-domains</key>
 \t<array>
-\t\t<string>applinks:d645-109-65-152-21.ngrok-free.app</string>
+\t\t<string>applinks:${host}</string>
 \t</array>
 </dict>
 </plist>
-`, `${github.workspace}/demo/demo/demo.entitlements`, {encoding: 'utf8'});
+`, path.join(__dirname, `../../demo/demo/demo.entitlements`), {encoding: 'utf8'});
+
+        resolve();
       }
     }, 500);
   })
