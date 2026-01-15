be5d61fadcd6064044cf81129c85d8d4c9f7901c
dianaKhortiuk-frontegg <diana.khortiuk@frontegg.com>
2026-01-15 16:49:29 +0100

    Tests fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.31.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.46.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
